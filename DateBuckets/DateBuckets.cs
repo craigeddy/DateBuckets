6c0b5f53ac69fb50e965ca45a79b607ea504b28f
Craig Eddy <craig.eddy@gmail.com>
2019-01-27 11:47:07 -0500

    Determine bucket size (days, weeks, months, quarters)

--- DateBuckets/DateBuckets.cs
+++ DateBuckets/DateBuckets.cs
@@ -1,7 +1,34 @@
-﻿namespace DateBuckets
+﻿using System;
+
+namespace DateBuckets
 {
-    public class DateBuckets
+    public class DateBucket
     {
-        
+        public enum DateRange
+        {
+            Days,
+            Weeks,
+            Months,
+            Quarters
+        }
+
+        /// <summary>
+        /// * tics should be
+        /// *
+        /// * days if range &lt; 1 month, 
+        /// * weeks if range &lt; 6 months, 
+        /// * months if range &lt; 24 months, 
+        /// * quarters if range &lt; 60 months
+        /// 
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
+        public static DateRange GetRange(DateTime start, DateTime end)
+        {
+            if (end < start.AddMonths(1)) return DateRange.Days;
+            if (end < start.AddMonths(6)) return DateRange.Weeks;
+            return end < start.AddMonths(24) ? DateRange.Months : DateRange.Quarters;
+        }
     }
 }
--- UnitTests/Tests.cs
+++ UnitTests/Tests.cs
@@ -1,6 +1,15 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DateBuckets;
 using NUnit.Framework;
+
+/*
+ * tics should be
+ *
+ * days if range < 1 month, 
+ * weeks if range < 6 months, 
+ * months if range < 24 months, 
+ * quarters if range < 60 months
+*/
 
 namespace UnitTests
 {
@@ -8,8 +17,14 @@ namespace UnitTests
     public class Tests
     {
         [Test]
-        public void TestMethod1()
+        [TestCase("1/3/2017", "12/1/2019", ExpectedResult = DateBucket.DateRange.Quarters)]
+        [TestCase("1/3/2017", "1/31/2017", ExpectedResult = DateBucket.DateRange.Days)]
+        [TestCase("1/3/2017", "5/31/2017", ExpectedResult = DateBucket.DateRange.Weeks)]
+        [TestCase("1/3/2017", "1/31/2018", ExpectedResult = DateBucket.DateRange.Months)]
+
+        public DateBucket.DateRange DateBucket_GetRange_ReturnsCorrectEnum(DateTime start, DateTime end)
         {
+            return DateBucket.GetRange(start, end);
         }
     }
 }
